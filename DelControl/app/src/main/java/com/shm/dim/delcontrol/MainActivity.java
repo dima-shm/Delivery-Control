734d099476d8b2944aacdc4fc0367331e1d0dd0b
Dima Shumanski <dima.shumanski@gmail.com>
2018-12-25 15:04:48 +0300

    Remove FloatingActionButton

--- DelControl/app/src/main/java/com/shm/dim/delcontrol/MainActivity.java
+++ DelControl/app/src/main/java/com/shm/dim/delcontrol/MainActivity.java
@@ -38,14 +38,6 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.tabs);
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
