baa912d1cb8230a182da51c1208bd65d87669123
Dima Shumanski <dima.shumanski@gmail.com>
2018-12-26 16:41:56 +0300

    Update FragmentMap
    Added google map key, permissionsn, and update fragment

--- DelControl/app/src/main/java/com/shm/dim/delcontrol/fragment/FragmentMap.java
+++ DelControl/app/src/main/java/com/shm/dim/delcontrol/fragment/FragmentMap.java
@@ -1,14 +1,23 @@
 package com.shm.dim.delcontrol.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.shm.dim.delcontrol.R;
 
 public class FragmentMap extends Fragment {
+
+    private SupportMapFragment mMapFragment;
 
     public FragmentMap() {
     }
@@ -24,4 +33,17 @@ public class FragmentMap extends Fragment {
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment);
+        mMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                LatLng latLng = new LatLng(53.901547, 27.553763);
+                googleMap.addMarker(new MarkerOptions().position(latLng).title("Minsk"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+        });
+    }
 }
