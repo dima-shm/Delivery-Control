aa0daa142ad62bed3c7e86de7a5801927210ecf4
Dima Shumanski <dima.shumanski@gmail.com>
2019-01-21 13:23:27 +0300

    Added Splash screen

---
+++ DelControl/app/src/main/java/com/shm/dim/delcontrol/activity/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.shm.dim.delcontrol.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.shm.dim.delcontrol.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 3 * 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startActivityAfterTimer(MainActivity.class, SPLASH_TIME);
+    }
+
+    private void startActivityAfterTimer(final Class<?> cls, int time) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mySuperIntent = new Intent(SplashActivity.this, cls);
+                startActivity(mySuperIntent);
+                finish();
+            }
+        }, time);
+    }
+
+}
