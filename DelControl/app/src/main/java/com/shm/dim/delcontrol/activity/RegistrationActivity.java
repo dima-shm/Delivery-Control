74a929d5618435dfd8bad8eb47c65dab6c86259f
Dima Shumanski <dima.shumanski@gmail.com>
2019-01-22 13:06:33 +0300

    Added Registration activity

---
+++ DelControl/app/src/main/java/com/shm/dim/delcontrol/activity/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package com.shm.dim.delcontrol.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.shm.dim.delcontrol.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
