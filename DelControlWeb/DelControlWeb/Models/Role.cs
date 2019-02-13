78ee72a703d4a6ff3ee2c6f6dc34ad98cc34e004
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-13 17:31:44 +0300

    Added role model

---
+++ DelControlWeb/DelControlWeb/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DelControlWeb.Models
+{
+    public class Role : IdentityRole
+    {
+        public Role() { }
+
+        public string Description { get; set; }
+    }
+}
