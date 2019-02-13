cc6e957aa6f9d3cd5f79d9a9692fa05f327221a3
Dima Shumanski <dima.shumanski@gmail.com>
2019-02-13 17:54:13 +0300

    Update context
    Added DbSets

--- DelControlWeb/DelControlWeb/Context/ApplicationContext.cs
+++ DelControlWeb/DelControlWeb/Context/ApplicationContext.cs
@@ -1,10 +1,25 @@
 ﻿using DelControlWeb.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace DelControlWeb.Context
 {
     public class ApplicationContext : IdentityDbContext<User>
     {
+        public DbSet<Company> Companies { get; set; }
+
+        public DbSet<LastCourierLocation> LastCourierLocations { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Delivery> Delivery { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderProducts> OrderProducts { get; set; }
+
         public ApplicationContext() : base("DelControlDB") { }
 
         public static ApplicationContext Create()
