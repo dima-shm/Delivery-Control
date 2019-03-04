8d86ad1b27658852b96624d88a85d261a14e2488
Dima Shumanski <dima.shumanski@gmail.com>
2019-03-04 11:11:09 +0300

    Add Migration

---
+++ DelControlWeb/DelControlWeb/Migrations/201903040810264_DataMigration.cs
@@ -0,0 +1,287 @@
+namespace DelControlWeb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Companies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        RegisterNumber = c.String(),
+                        RegisterDate = c.DateTime(nullable: false),
+                        TaxpayerNumber = c.String(),
+                        Address = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.CompanyCourierStatuses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CompanyId = c.Int(),
+                        CourierStatusId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Companies", t => t.CompanyId)
+                .ForeignKey("dbo.CourierStatus", t => t.CourierStatusId)
+                .Index(t => t.CompanyId)
+                .Index(t => t.CourierStatusId);
+            
+            CreateTable(
+                "dbo.CourierStatus",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.CompanyOrderStatuses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CompanyId = c.Int(),
+                        OrderStatusId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Companies", t => t.CompanyId)
+                .ForeignKey("dbo.OrderStatus", t => t.OrderStatusId)
+                .Index(t => t.CompanyId)
+                .Index(t => t.OrderStatusId);
+            
+            CreateTable(
+                "dbo.OrderStatus",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Phone = c.String(),
+                        Address = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Deliveries",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Address = c.String(),
+                        Date = c.DateTime(nullable: false),
+                        Time = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.LastCourierLocations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CourierId = c.String(maxLength: 128),
+                        Latitude = c.Single(nullable: false),
+                        Longitude = c.Single(nullable: false),
+                        Speed = c.Single(nullable: false),
+                        Time = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.CourierId)
+                .Index(t => t.CourierId);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CompanyId = c.Int(),
+                        Surname = c.String(),
+                        Name = c.String(),
+                        Patronymic = c.String(),
+                        Email = c.String(maxLength: 256),
+                        Phone = c.String(),
+                        Address = c.String(),
+                        Status = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Companies", t => t.CompanyId)
+                .Index(t => t.CompanyId)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.OrderProducts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        OrderId = c.Int(),
+                        ProductId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Orders", t => t.OrderId)
+                .ForeignKey("dbo.Products", t => t.ProductId)
+                .Index(t => t.OrderId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CompanyId = c.Int(),
+                        CustomerId = c.Int(),
+                        DeliveryId = c.Int(),
+                        CourierId = c.String(maxLength: 128),
+                        Status = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Companies", t => t.CompanyId)
+                .ForeignKey("dbo.AspNetUsers", t => t.CourierId)
+                .ForeignKey("dbo.Customers", t => t.CustomerId)
+                .ForeignKey("dbo.Deliveries", t => t.DeliveryId)
+                .Index(t => t.CompanyId)
+                .Index(t => t.CustomerId)
+                .Index(t => t.DeliveryId)
+                .Index(t => t.CourierId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Descriotion = c.String(),
+                        Price = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Description = c.String(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.OrderProducts", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.OrderProducts", "OrderId", "dbo.Orders");
+            DropForeignKey("dbo.Orders", "DeliveryId", "dbo.Deliveries");
+            DropForeignKey("dbo.Orders", "CustomerId", "dbo.Customers");
+            DropForeignKey("dbo.Orders", "CourierId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Orders", "CompanyId", "dbo.Companies");
+            DropForeignKey("dbo.LastCourierLocations", "CourierId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUsers", "CompanyId", "dbo.Companies");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.CompanyOrderStatuses", "OrderStatusId", "dbo.OrderStatus");
+            DropForeignKey("dbo.CompanyOrderStatuses", "CompanyId", "dbo.Companies");
+            DropForeignKey("dbo.CompanyCourierStatuses", "CourierStatusId", "dbo.CourierStatus");
+            DropForeignKey("dbo.CompanyCourierStatuses", "CompanyId", "dbo.Companies");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Orders", new[] { "CourierId" });
+            DropIndex("dbo.Orders", new[] { "DeliveryId" });
+            DropIndex("dbo.Orders", new[] { "CustomerId" });
+            DropIndex("dbo.Orders", new[] { "CompanyId" });
+            DropIndex("dbo.OrderProducts", new[] { "ProductId" });
+            DropIndex("dbo.OrderProducts", new[] { "OrderId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.AspNetUsers", new[] { "CompanyId" });
+            DropIndex("dbo.LastCourierLocations", new[] { "CourierId" });
+            DropIndex("dbo.CompanyOrderStatuses", new[] { "OrderStatusId" });
+            DropIndex("dbo.CompanyOrderStatuses", new[] { "CompanyId" });
+            DropIndex("dbo.CompanyCourierStatuses", new[] { "CourierStatusId" });
+            DropIndex("dbo.CompanyCourierStatuses", new[] { "CompanyId" });
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Products");
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderProducts");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.LastCourierLocations");
+            DropTable("dbo.Deliveries");
+            DropTable("dbo.Customers");
+            DropTable("dbo.OrderStatus");
+            DropTable("dbo.CompanyOrderStatuses");
+            DropTable("dbo.CourierStatus");
+            DropTable("dbo.CompanyCourierStatuses");
+            DropTable("dbo.Companies");
+        }
+    }
+}
