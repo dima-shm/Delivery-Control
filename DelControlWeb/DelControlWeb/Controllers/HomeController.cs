62dd2a5bc10778a3b0c25ec9e702d8d7744267f8
Dima Shumanski <dima.shumanski@gmail.com>
2019-03-29 11:08:17 +0300

    Add HomeController and index view

---
+++ DelControlWeb/DelControlWeb/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace DelControlWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
