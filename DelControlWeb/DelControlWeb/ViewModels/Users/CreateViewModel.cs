2bd554025e092e308f20d807c4d8c088946fc34d
Dima Shumanski <dima.shumanski@gmail.com>
2019-04-08 20:20:22 +0300

    Added user viewmodels and update views

---
+++ DelControlWeb/DelControlWeb/ViewModels/Users/CreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DelControlWeb.ViewModels.Users
+{
+    public class CreateViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
