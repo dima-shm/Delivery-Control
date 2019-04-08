db040d06412418f6be692a47fdd122f453621460
Dima Shumanski <dima.shumanski@gmail.com>
2019-04-08 20:54:50 +0300

    Added users EditViewModel

---
+++ DelControlWeb/DelControlWeb/ViewModels/Users/EditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DelControlWeb.ViewModels.Users
+{
+    public class EditViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
